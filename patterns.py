6d85fd472176cc7c38975d10482cb9e1ee984774
NandiniBodagala <143414395+NandiniBodagala@users.noreply.github.com>
2024-12-02 16:14:09 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,36 @@
+# for i in range(5):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+#
+# for i in range(5):#rows
+#     for j in range(5-i):#columns
+#         print("*",end=" ")
+#     print()
+
+# for i in range(5):
+#     for k in range(5-i):
+#         print(end=' ')
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+# row = 6
+# for i in range(1,row+1):#rows
+#     for j in range(1,i+1):#columns
+#         print(j,end=" ")
+#     print()
+
+# n =5
+# count=1
+# for i in range(1,n+1):#rows
+#     for j in range(1,i+1):#columns
+#         print(count,end=" ")
+#         count += 1
+#     print(end="\n")
+
+
+
+
+
